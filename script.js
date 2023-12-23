32e57518a7a51a9be9ad0bd0a0766a20161f804d
frankMarcelino <frank.silva.ti@gmail.com>
2023-12-23 14:04:34 -0300

    terminei projetinho

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
 
+
+
 function insert(num) {
-    document.querySelector('.resultado').innerHTML += num
-    
+    document.querySelector('.resultado').innerHTML += num    
+   
 }
 
+function apagar() {
+    let tela = document.querySelector('.resultado').innerHTML
+    document.querySelector('.resultado').innerHTML = tela.substring(0, tela.length -1)
+}
 
 
 function limpar() {
     document.querySelector('.resultado').innerHTML = ""
+}
+
+function calcular() {
+    let tela = document.querySelector('.resultado').innerHTML
+    if (tela) {
+        document.querySelector('.resultado').innerHTML = eval(tela)
+    }
 }
